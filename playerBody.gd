2faec24a7338bf1d5b31d65b78206c420af523f2
tuto193 <carlos.a.parra.f@gmail.com>
2018-07-11 22:13:50 +0200

    Added more player movement

--- playerBody.gd
+++ playerBody.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
 # class member variables go here, for example:
-var UP = Vector2( 0, 1 )
-var GRAVITY = 80
-var MAX_SPEED_FALL = 500
-var MAX_SPEED_X = 300
-var MAX_ACCEL_X = 20
-var JUMP_SPEED = 400
+const UP = Vector2( 0, 1 )
+const GRAVITY = 80
+const MAX_SPEED_FALL = 500
+const MAX_SPEED_X = 300
+const ACCEL_X = 20
+const JUMP_SPEED = 400
 var player_speed = Vector2( 0, 0 )
 
 func _ready():
@@ -23,8 +23,16 @@ func _process(delta):
 	if( !is_on_floor() ):
 		if( player_speed.y < MAX_SPEED_FALL ):
 			player_speed.y += GRAVITY
-	
-	if( InputEvent.is_action_just_pressed("player_UP") ):
+	# Here is the player movement handled
+	if InputEvent.is_action_just_pressed("player_UP"):
+		if is_on_floor():
+			player_speed.y = JUMP_SPEED
+	elif InputEvent.is_action_pressed("player_RIGHT"):
+		if player_speed.x < MAX_SPEED_X:
+			player_speed.x += ACCEL_X
+	elif InputEvent.is_action_pressed( "player_LEFT" ):
+		if player_speed.x > -MAX_SPEED_X:
+			player_speed.x -= ACCEL_X
 
 
 	pass
